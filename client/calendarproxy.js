bb3f024960e10e2aaeab5669f7a75142ac772f3b
Charles Julian Knight <charles@rabidaudio.com>
2014-01-28 23:34:17 -0500

    Working client code

--- client/calendarproxy.js
+++ client/calendarproxy.js
@@ -1,11 +1,26 @@
 var PROXY_URL = "http://calendar-proxy.herokuapp.com";
 var GOOGLE_PATH = "/google";
-
-function get_calendars(urls, callback){
+/*
+    options:
+        start:      JSONified date to start looking for events. defaults to today
+        end:        upper limit to look for events. defaults to 2 weeks from now
+        all:        include hidden and deleted items. defaults to false
+        orderby:    lastmodified, starttime (default starttime)
+        sortorder:  ascending,ascend,a,descending,descend,d (default ascending)
+*/
+function get_calendars(urls, options, callback){
     var cb = (typeof callback == 'function') ? callback : new Function(callback);
+    querystring="";
+    if(options.start    != undefined)   querystring=querystring+ "&start="     +encodeURIComponent((new Date(options.start)).toISOString());
+    if(options.end      != undefined)   querystring=querystring+ "&end="       +encodeURIComponent((new Date(options.end)).toISOString());
+    if(options.all      != undefined)   querystring=querystring+ "&all="       +encodeURIComponent(options.all);
+    if(options.orderby  != undefined)   querystring=querystring+ "&orderby="   +encodeURIComponent(options.orderby);
+    if(options.sortorder!= undefined)   querystring=querystring+ "&sortorder=" +encodeURIComponent(options.sortorder);
+    
+    //console.log( PROXY_URL+GOOGLE_PATH+"?"+"url="+ encodeURIComponent(urls[0])+querystring );
     var results = [];
     for(var i=0; i<urls.length;i++){
-        $.ajax( PROXY_URL+GOOGLE_PATH+"?"+"url="+ encodeURIComponent(urls[i]))
+        $.ajax( PROXY_URL+GOOGLE_PATH+"?"+"url="+ encodeURIComponent(urls[i])+querystring)
             .done(function(data){
                 //console.log(data);
                 results.push(data);
@@ -15,8 +30,6 @@ function get_calendars(urls, callback){
                 results.push(errorThrown);
             })
             .always(function(){
-                    console.log(results);
-                    console.log("executing callback");
                     cb(results);
             });
     }
